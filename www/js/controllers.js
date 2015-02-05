a3cf0b33f5a769d7f85be0d1fc186f3e9a7abee9
Kal Wolfson <kalwolfson@hotmail.com>
2015-02-05 16:20:08 +0200

    This is the completed Project to The tangent test requiements

---
+++ www/js/controllers.js
@@ -0,0 +1,41 @@
+﻿angular.module("starter.controllers", [])
+    .controller("AppCtrl", function($scope) {
+        $scope = $scope;
+    })
+    .controller("stallListCtrl", function ($scope, Stalls) {
+        console.log(Stalls.all());
+        $scope.stalls = Stalls.all();
+    })
+    .controller("stallDetailCtrl", function ($scope, $stateParams, Stalls) {
+        console.log($stateParams.stallId);
+        $scope.stall = Stalls.get($stateParams.stallId);
+    })
+    .controller('MapCtrl', function ($scope, $ionicLoading) {
+        $scope.mapCreated = function (map) {
+            $scope.map = map;
+        };
+
+        $scope.centerOnMe = function () {
+            console.log("Centering");
+            if (!$scope.map) {
+                return;
+            }
+
+            $scope.loading = $ionicLoading.show({
+                content: 'Getting current location...',
+                showBackdrop: false
+            });
+
+            navigator.geolocation.getCurrentPosition(function (pos) {
+                console.log('Got pos', pos);
+                $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
+                $scope.loading.hide();
+            }, function (error) {
+                alert('Unable to get location: ' + error.message);
+            });
+        };
+    });
+
+    
+
+   
---
+++ www/js/services.js
@@ -0,0 +1,41 @@
+﻿angular.module('starter.services', [])
+    .factory('Stalls', function() {
+        var stalls = [{
+                id: 0,
+                name: 'BMW',
+                sub: 'German Car Manufacturer',
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+                logo: 'img/1.png',
+                ImageLogo:'img/BMW.jpg',
+            }, {
+                id: 1,
+                name: 'Toyota',
+                sub: 'Japanese Car Manufacturer',
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+                logo: 'img/2.png',
+                ImageLogo: 'img/Toyota.jpg',
+            }, {
+                id: 2,
+                name: 'VW',
+                sub: 'German Car Manufacturer',
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+                logo: 'img/3.png',
+                ImageLogo: 'img/VW.jpg',
+            }, {
+                id: 3,
+                name: 'Ford',
+                sub: 'American Car Manufacturer',
+                description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. ",
+                logo: 'img/4.png',
+                ImageLogo: 'img/Ford.jpg',
+            }];
+        return {
+            all: function() {
+                return stalls;
+            },
+            get: function(stallId) {
+                //simple index lookup
+                return stalls[stallId];
+            }
+        };
+    });
